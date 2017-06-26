e958a497eb1aef1d719ea587523e688d26df6412
cmontoya13 <chris@chrismontoya.com>
2017-06-25 19:06:21 -0700

    final burgers - requires Heroku

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,39 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function(req, res) {
+  burger.create([
+    "burger_name"
+  ], [
+    req.body.burger_name
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+// Export routes (server.js)
+module.exports = router;
+
